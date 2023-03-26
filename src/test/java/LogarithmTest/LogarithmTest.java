f55336ef3dc50032e91ca121cca699ea73b8c39c
Daniil <twilightnotch@gmail.com>
2023-03-26 21:13:49 +0300

    Add test for algorithm

--- src/test/java/LogarithmTest/LogarithmTest.java
+++ src/test/java/LogarithmTest/LogarithmTest.java
@@ -3,23 +3,29 @@ package LogarithmTest;
 import csv.Printer;
 import functions.logarithms.Ln;
 import functions.logarithms.LogX;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.io.FileNotFoundException;
+
 public class LogarithmTest {
     private final double accuracy = 1 / 1e5;
-    private Ln ln = new Ln();
-    private LogX logX = new LogX();
-    private Printer printer = new Printer();
+    private final Ln ln = new Ln();
+    private final LogX logX = new LogX();
+    private final Printer printer = new Printer();
 
     @ParameterizedTest
     @DisplayName("Тестирование натурального логарифма")
-    @CsvFileSource(resources = "/file.csv")
-    void lnTest(double number, double trueAnswer) {
+    @CsvFileSource(resources = "/input/lnInput.csv")
+    void lnTest(double number, double trueAnswer) throws FileNotFoundException {
         try {
             Double answer = ln.calculate(number, accuracy);
-            printer.csvPrint(number,answer,"");
+            printer.csvPrint(number, answer, "src/test/resources/output/lnOutput.csv");
+            Assertions.assertEquals(trueAnswer, answer, 0.1);
+        } catch (ArithmeticException e) {
+            Assertions.assertEquals("X должен быть больше 0!", e.getMessage());
         }
     }
 }

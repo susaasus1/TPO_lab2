086190d29d489b446bae07091c502034c4c19f1d
Daniil <twilightnotch@gmail.com>
2023-03-27 00:29:10 +0300

    Add new functional

--- src/test/java/FunctionTest.java
+++ src/test/java/FunctionTest.java
@@ -1,4 +1,23 @@
+import functions.function.TrigonometryFunctionCalculator;
+import functions.logarithms.Ln;
+import functions.logarithms.LogX;
+import functions.logarithms.LogarithmFunction;
+import org.junit.jupiter.api.BeforeAll;
+import org.mockito.Mockito;
+
 public class FunctionTest {
     private static final double accuracy = 1 / 1e5;
+    private static Ln lnMock;
+    private static LogX logXMock;
+    private static LogarithmFunction logarithmFunctionMock;
+    private static TrigonometryFunctionCalculator trigonometryFunctionCalculatorMock;
+
+    @BeforeAll
+    static void init() {
+        lnMock = Mockito.mock(Ln.class);
+        logXMock = Mockito.mock(LogX.class);
+        logarithmFunctionMock = Mockito.mock(LogarithmFunction.class);
+        trigonometryFunctionCalculatorMock = Mockito.mock(TrigonometryFunctionCalculator.class);
+    }
 
 }
